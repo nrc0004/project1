d4697b4e7924dfdb1e1be27cc39713e0b71211ef
Natasha Clark <Natasharclark@gmail.com>
2017-03-29 14:27:28 -0400

    main structure completed

--- app.js
+++ app.js
@@ -35,83 +35,120 @@ next.on("click", function(){
 
   var random = catagories[Math.floor(Math.random()*catagories.length)]
   console.log (random)
-  
   card.append(bottom)
 
-  if (random === catagories[0]) {
-    var playerAnswer = $("#playerAnswer").val()
-    var questions= "How many kids?"
-    var choices = ["1", "2", "3", "4"]
-    var answer = "2"
-    //change background to reflect category
-    display.css("background-image", "url('http://i.imgur.com/DnXrgG2.jpg')")
-    //questions display
+      if (random === catagories[0]) {
+        var playerAnswer = $("#playerAnswer").val()
+        var questions= "How many kids?"
+        var choices = ["1", "2", "3", "4"]
+        var answer = "2"
 
-    question.html("<h2>"+ questions+ "</h2>")
-    for ( i= 0; i < choices.length; i++) {
-      question.append ("<ul>" + choices[i] + "</ul>")
-    }
+        //change background to reflect category
+        display.css("background-image", "url('http://i.imgur.com/DnXrgG2.jpg')")
+        //questions display
 
-    submit.on("click", function(){
-      if (playerAnswer === null) {
-        alert("You must enter an answer. Please type your answer in the space provided")
-      } else{
-        if (playerAnswer === answer) {
-          question.html("<h2> Correct!</h2>")
-          $("#card1").css("opacity", "1")
-        } else {
-          question.html("<h2> Sorry that is incorrect.</h2> <p>Press Next to keep playing</p>")
+        question.html("<h2>"+ questions+ "</h2>")
+        for ( i= 0; i < choices.length; i++) {
+          question.append ("<ul>" + choices[i] + "</ul>")
+        }
+
+        submit.on("click", function(){
+          if (playerAnswer === null) {
+            alert("You must enter an answer. Please type your answer in the space provided")
+          } else{
+            if (playerAnswer === answer) {
+              question.html("<h2> Correct!</h2>")
+              $("#card1").css("opacity", "1")
+            } else {
+              question.html("<h2> Sorry that is incorrect.</h2> <p>Press Next to keep playing</p>")
+            }
+          }
+        }
+      )
+    } else if (random === catagories[1]){
+      var playerAnswer = $("#playerAnswer").val()
+      var questions= "SCIENCE"
+      var choices = ["1", "2", "3", "4"]
+      var answer = "2"
+
+      //change background to reflect category
+      display.css("background-image", "url('http://i.imgur.com/r1nBBTD.jpg')")
+      //questions display
+
+      question.html("<h2>"+ questions+ "</h2>")
+      for ( i= 0; i < choices.length; i++) {
+        question.append ("<ul>" + choices[i] + "</ul>")
+      }
+
+      submit.on("click", function(){
+        if (playerAnswer === null) {
+          alert("You must enter an answer. Please type your answer in the space provided")
+        } else{
+          if (playerAnswer === answer) {
+            question.html("<h2> Correct!</h2>")
+            $("#card1").css("opacity", "1")
+          } else {
+            question.html("<h2> Sorry that is incorrect.</h2> <p>Press Next to keep playing</p>")
+          }
         }
       }
-    })
-  }
-  }
-)
+    )
+    } else if (random === catagories[2]){
+      var playerAnswer = $("#playerAnswer").val()
+      var questions= "LITTT"
+      var choices = ["1", "2", "3", "4"]
+      var answer = "2"
 
+      //change background to reflect category
+      display.css("background-image", "url('http://i.imgur.com/CTCiHwq.jpg')")
+      //questions display
 
-  //run when next button is clicked
+      question.html("<h2>"+ questions+ "</h2>")
+      for ( i= 0; i < choices.length; i++) {
+        question.append ("<ul>" + choices[i] + "</ul>")
+      }
 
-// <---game logic---->
+      submit.on("click", function(){
+        if (playerAnswer === null) {
+          alert("You must enter an answer. Please type your answer in the space provided")
+        } else{
+          if (playerAnswer === answer) {
+            question.html("<h2> Correct!</h2>")
+            $("#card1").css("opacity", "1")
+          } else {
+            question.html("<h2> Sorry that is incorrect.</h2> <p>Press Next to keep playing</p>")
+          }
+        }
+      }
+    )
+    } else if(random === catagories[3]){
+        var playerAnswer = $("#playerAnswer").val()
+        var questions= "History"
+        var choices = ["1", "2", "3", "4"]
+        var answer = "2"
 
+        //change background to reflect category
+        display.css("background-image", "url('http://i.imgur.com/MAntXg7.jpg')")
+        //questions display
 
+        question.html("<h2>"+ questions+ "</h2>")
+        for ( i= 0; i < choices.length; i++) {
+          question.append ("<ul>" + choices[i] + "</ul>")
+        }
 
-
-
-
-
+        submit.on("click", function(){
+          if (playerAnswer === null) {
+            alert("You must enter an answer. Please type your answer in the space provided")
+          } else{
+            if (playerAnswer === answer) {
+              question.html("<h2> Correct!</h2>")
+              $("#card1").css("opacity", "1")
+            } else {
+              question.html("<h2> Sorry that is incorrect.</h2> <p>Press Next to keep playing</p>")
+            }
+          }
+        }
+      )
+    }
+  })
 })
-
-
-
-//<--game logic -->
-
-//while total < 4
-// if 1 dislay history question
-  //if correct? display correct message,  light up topic center,1 to total  and promp to play again
-  //if wrong? display incorrect message and prompt to play again
-//if 2 pop
-  //if correct? display correct message,  light up topic center,1 to total  and promp to play again
-  //if wrong? display incorrect message and prompt to play again
-//if 3 sport
-//if 4 science
-
-//game logic
-// while (total<4) {
-//   if () {}
-//
-//
-// }
-//when submit button is clicked
-//   submit.on("click", function(){
-//     console.log(playerAnswer)
-//   }
-
-
-// while(popFlag == true && litFlag == true && hisFlag == true && sciFlag ==true ) {
-//   question.html("<h2>Congratulations, you won!</h2>")
-//   $(".question").css("background-color", "green")
-// }
-
-
-// )
-// })
