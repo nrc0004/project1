ee2ab13e9d1431d06af8c0e35d10ed750a25114e
Natasha Clark <Natasharclark@gmail.com>
2017-03-29 11:29:41 -0400

    game logic added

---
+++ app.js
@@ -0,0 +1,95 @@
+$(document).ready(function(){
+
+  var total = 0
+  var next = $(".next")
+  var pop = $("#card1")
+  var science = $("#card2")
+  var poli = $("#card3")
+  var his = $("#card4")
+  var question = $(".question")
+  var submit = $(".submit")
+  var playerAnswer = $("#playerAnswer").val()
+  var popFlag = false
+  var litFlag = false
+  var hisFlag = false
+  var sciFlag = false
+  var catagories= ["Pop Culture", "Science", "Literature", "History" ]
+  var rand = catagories[Math.floor(Math.random()*catagories.length)]
+
+var question = next.on("click", function(){
+  var rand = catagories[Math.floor(Math.random()*catagories.length)]
+  console.log (rand)
+
+  if (rand === catagories[0]) {
+    var questions= "How many kids?"
+    var choices = ["1", "2", "3", "4"]
+    var answer = "2"
+
+    //questions display
+    question.html("<h2>"+ questions+ "</h2>")
+        for ( i= 0; i < choices.length; i++) {
+        question.append ("<li>" + choices[i] + "</li>")
+        }
+
+    submit.on("click", function(){
+      if (playerAnswer === null  || playerAnswer === "" ) {
+        alert("You must enter an answer. Please type your answer in the space provided")
+      } else{
+        if (playerAnswer === answer) {
+            question.html("<h2> Correct!</h2>")
+            $("#card1").css("opacity", "1")
+        } else {
+          question.html("<h2> Sorry that is incorrect.</h2> <p>Press Next to keep playing</p>")
+        }
+      }
+    })
+  }
+}
+)
+
+  //run when next button is clicked
+
+// <---game logic---->
+
+
+
+
+
+
+
+})
+
+
+
+//<--game logic -->
+
+//while total < 4
+// if 1 dislay history question
+  //if correct? display correct message,  light up topic center,1 to total  and promp to play again
+  //if wrong? display incorrect message and prompt to play again
+//if 2 pop
+  //if correct? display correct message,  light up topic center,1 to total  and promp to play again
+  //if wrong? display incorrect message and prompt to play again
+//if 3 sport
+//if 4 science
+
+//game logic
+// while (total<4) {
+//   if () {}
+//
+//
+// }
+//when submit button is clicked
+//   submit.on("click", function(){
+//     console.log(playerAnswer)
+//   }
+
+
+// while(popFlag == true && litFlag == true && hisFlag == true && sciFlag ==true ) {
+//   question.html("<h2>Congratulations, you won!</h2>")
+//   $(".question").css("background-color", "green")
+// }
+
+
+// )
+// })
