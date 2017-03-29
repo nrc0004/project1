94e1b67ed0d655a4c0719bce0ed3e59f7aabe629
Natasha Clark <Natasharclark@gmail.com>
2017-03-29 13:32:32 -0400

    css added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 
   var total = 0
+  var instructions= $(".instructions")
+  var about = $(".about")
   var next = $(".next")
   var pop = $("#card1")
   var science = $("#card2")
@@ -8,31 +10,50 @@ $(document).ready(function(){
   var his = $("#card4")
   var question = $(".question")
   var submit = $(".submit")
-  var playerAnswer = $("#playerAnswer").val()
+  var display = $(".display")
+  var bottom = $(".bottom")
+  var card = $(".card")
   var popFlag = false
   var litFlag = false
   var hisFlag = false
   var sciFlag = false
   var catagories= ["Pop Culture", "Science", "Literature", "History" ]
 
+// <---Intro and how to--->
+about.on("click", function(){
+    question.html("<h1> This is a test</h1><p>more testing</p>")
+    bottom.remove()
+})
+
+instructions.on("click", function(){
+    question.html("<h1> THIS is a test</h1><p>more testing</p>")
+    bottom.remove()
+})
+
+
 next.on("click", function(){
 
   var random = catagories[Math.floor(Math.random()*catagories.length)]
   console.log (random)
+  
+  card.append(bottom)
 
   if (random === catagories[0]) {
+    var playerAnswer = $("#playerAnswer").val()
     var questions= "How many kids?"
     var choices = ["1", "2", "3", "4"]
     var answer = "2"
-
+    //change background to reflect category
+    display.css("background-image", "url('http://i.imgur.com/DnXrgG2.jpg')")
     //questions display
+
     question.html("<h2>"+ questions+ "</h2>")
     for ( i= 0; i < choices.length; i++) {
-      question.append ("<li>" + choices[i] + "</li>")
+      question.append ("<ul>" + choices[i] + "</ul>")
     }
 
     submit.on("click", function(){
-      if (playerAnswer === null  || playerAnswer === "" ) {
+      if (playerAnswer === null) {
         alert("You must enter an answer. Please type your answer in the space provided")
       } else{
         if (playerAnswer === answer) {
